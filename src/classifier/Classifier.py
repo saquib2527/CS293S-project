1a9dd2535b37b122aef45548394708155849d097
Nazmus Saquib <saquib2527@yahoo.com>
2019-01-26 10:25:45 -0800

    add penman equation

--- src/classifier/Classifier.py
+++ src/classifier/Classifier.py
@@ -1,10 +1,13 @@
 import math
 from operator import itemgetter
 
-from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix
+from sklearn import preprocessing
+
+from sklearn import linear_model
 from sklearn.neural_network import MLPRegressor
+
+from sklearn.metrics import mean_squared_error, r2_score, confusion_matrix
 
 import pandas as pd
 import numpy as np
@@ -81,18 +84,27 @@ class Classifier:
                 break
         print('correct: {0} incorrect: {1}'.format(correct, incorrect))
 
-    def linear_regression(self):
+    def regression(self):
         #self.df.HlyAirTmp = (self.df.HlyAirTmp - 32) * (5.0 / 9.0) + 273
         #self.df.HlyNetRad = (self.df.HlyNetRad / (60 * 24)) * 697.3
         #self.df.HlyWindSpd = self.df.HlyWindSpd * 0.44704
         train, test = train_test_split(self.df, test_size=0.2)
-        #reg = MLPRegressor()
-        reg = linear_model.LinearRegression()
+
+        reg = MLPRegressor()
+        scaler = preprocessing.StandardScaler()
+        X_train_scaled = scaler.fit_transform(train[self.features])
+        X_test_scaled = scaler.fit_transform(test[self.features])
+        reg.fit(X_train_scaled, train[self.target])
+        predictedY = reg.predict(X_test_scaled)
+
+        #reg = linear_model.LinearRegression()
         #reg = linear_model.Ridge(alpha=0.5)
         #reg = linear_model.Lasso(alpha=0.1)
         #reg = linear_model.BayesianRidge()
-        reg.fit(train[self.features], train[self.target])
-        predictedY = reg.predict(test[self.features])
+
+        #reg.fit(train[self.features], train[self.target])
+        #predictedY = reg.predict(test[self.features])
+
         for x in range(len(predictedY)):
             if predictedY[x] < 0:
                 predictedY[x] = 0
@@ -123,6 +135,37 @@ class Classifier:
 
     def feet_to_meter(self, elevation):
         return (elevation * 0.3048)
+
+    def penman(self):
+        for x in range(self.df.shape[0]):
+            row = self.df.loc[x]
+            station_number = row['StationNbr']
+            T_a = self.fahrenheit_to_celsius(row['HlyAirTmp'])
+            e_s = 0.6108 * math.exp((17.27 * T_a) / (T_a + 237.3))
+            delta = (4099 * e_s) / ((T_a + 237.3) ** 2)
+            R_n = row['HlyNetRad'] * (11.63 * 3.6) / 24 #conversion to MJm^-2h^-1, equation has calculated value for HlyNetRad
+            A = self.feet_to_meter(self.stations[self.stations['StationNbr'] == station_number]['Elevation'].iloc[0]) #elevation of the station above mean sea level
+            P = 101.3 * (((293 - 0.0065 * A) / 293) ** 5.26) #barometric pressure in KPa
+            Lambda = 2.501 - 0.002361 * T_a
+            Y = 0.00163 * (P / Lambda)
+            e_a = e_s * (row['HlyRelHum'] / 100.0) #notice this is HlyVapPres once expressed in KPa, varified it works!
+            if R_n > 0:
+                G = 0.1 * R_n
+                C_d = 0.24
+            else:
+                G = 0.5 * R_n
+                C_d = 0.96
+            U_2 = row['HlyWindSpd'] * 0.44704
+            first_part = (delta * (R_n - G)) / (Lambda * (delta + Y * (1 + C_d * U_2)))
+            second_part = (Y * (37 / (T_a + 273.16)) * U_2 * (e_s - e_a)) / (delta + Y * (1 + C_d * U_2))
+            ET_o = first_part + second_part
+            #if ET_o < 100:
+            #    ET_o = 0
+            #elif ET_o < 300:
+            #    ET_o = 0.01
+            print('{0} {1}'.format(ET_o, row['HlyEto']))
+            if x == 100:
+                break
 
     def CIMIS_penman(self):
         #print(self.df[(self.df['StationNbr'] == 5) & (self.df['Date'] == '2014-01-01') & (self.df['Hour'] == 100)]['HlyAirTmp'])
@@ -156,8 +199,8 @@ class Classifier:
 
 if __name__ == '__main__':
     c = Classifier()
-    c.CIMIS_penman()
+    #c.penman()
     #c.geographically_nearest_classifier(1)
     #c.populate_nearest_neighbors()
-    #c.linear_regression()
+    c.regression()
     #c.print_stats()
